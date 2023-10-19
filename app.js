003fe1373f0fe4cfae6de9e319fbca4aeeb5c117
Ivan Derda <smradoch12@gmail.com>
2023-10-20 00:03:49 +0200

    improving properties

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     // template: '<h2>I am the template</h2>'
     data() {
         return {
-            title: 'The final empuire'
+            title: 'The final empuire',
+            author: 'Brandon Sanderson',
+            age: 45
         }
     }
 })
