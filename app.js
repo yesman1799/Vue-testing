1d2be4a4344eaa356f683f281b32fac863bc17a3
Ivan Derda <smradoch12@gmail.com>
2023-10-20 00:01:34 +0200

    testing cdn vue properties

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello, vue')
+const app = Vue.createApp({
+    // template: '<h2>I am the template</h2>'
+    data() {
+        return {
+            title: 'The final empuire'
+        }
+    }
+})
+
+
+app.mount('#app')
